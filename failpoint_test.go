eef8d28e57242f36e153100dcba8805b724a5440
Lonng <chris@lonng.org>
2019-04-10 17:08:37 +0800

    failpoint: add unit test for WithHook (#7)

---
+++ failpoint_test.go
@@ -0,0 +1,39 @@
+package failpoint_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/pingcap/check"
+	"github.com/pingcap/failpoint"
+)
+
+func TestFailpoint(t *testing.T) {
+	TestingT(t)
+}
+
+var _ = &failpointSuite{}
+
+type failpointSuite struct{}
+
+func (s *failpointSuite) TestWithHook(c *C) {
+	ctx := failpoint.WithHook(context.Background(), func(ctx context.Context, fpname string) bool {
+		return false
+	})
+	ok, val := failpoint.Eval("unit-test", ctx)
+	c.Assert(ok, IsFalse)
+	c.Assert(val, IsNil)
+
+	ctx = failpoint.WithHook(context.Background(), func(ctx context.Context, fpname string) bool {
+		return true
+	})
+	err := failpoint.Enable("TestWithHook-test-1", "return(1)")
+	defer func() {
+		err := failpoint.Disable("TestWithHook-test-1")
+		c.Assert(err, IsNil)
+	}()
+	c.Assert(err, IsNil)
+	ok, val = failpoint.Eval("TestWithHook-test-1", ctx)
+	c.Assert(ok, IsTrue)
+	c.Assert(val.(int), Equals, 1)
+}
